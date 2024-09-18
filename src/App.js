5ddfadbfae8c4dc157f2b430587a71327d9d341a
Mrinmoy Adhikary <63409502+mrinmoy96@users.noreply.github.com>
2024-09-18 11:19:22 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+// import './App.css';
+import Timer from "./Timer/Timer.jsx"
+
+function App() {
+  return (
+    <div className="App">
+      {/* <h1>Stopwatch</h1> */}
+      <Timer/>        
+    </div>
+  );
+}
+
+export default App;
